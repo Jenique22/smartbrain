db8ebca14c904a114f307948bd118202da11c1e8
Jenique Knoesen <70643720+Jenique22@users.noreply.github.com>
2021-10-11 12:51:59 +0200

    first commit

---
+++ src/Components/Rank/Rank.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+const Rank= () => {
+    return (
+       <div>
+         
+      <div className='white f3'>
+        {'Jenique your curent rank is ...'}
+     </div> 
+     <div className='white f1'>
+        {'#5'}
+     </div> 
+          
+    </div>
+    
+    );
+}
+
+export default Rank;
