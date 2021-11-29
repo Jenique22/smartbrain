3f8e3398ff43021dc0d66b46449bc9a4bd3191ed
Jenique Knoesen <Jenique82@gmail.com>
2021-11-29 10:50:22 +0200

    fixed image url

--- src/App.js
+++ src/App.js
@@ -87,7 +87,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('https://desolate-eyrie-52418.herokuapp.com:3000/image', {
+          fetch('https://desolate-eyrie-52418.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
